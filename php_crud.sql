72fa013932d85ae41ada642df6742e0cfaf5e894
Sudaba Solaimankhil <52736711+SudabaSolaimankhil@users.noreply.github.com>
2020-04-25 02:11:47 +0430

    Add files via upload

---
+++ php_crud.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2020 at 11:05 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crudtable`
+--
+
+CREATE TABLE `crudtable` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phonenumber` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `crudtable`
+--
+
+INSERT INTO `crudtable` (`id`, `name`, `lastname`, `email`, `phonenumber`) VALUES
+(2, 'karim', 'akbari', 'karim@gmail.com', '1234567890'),
+(3, 'vali ahmad', 'karimy', 'vali@gmail.com', '0987654321');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `crudtable`
+--
+ALTER TABLE `crudtable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `crudtable`
+--
+ALTER TABLE `crudtable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
